3e332b7aa9c6b82dfcf53fc815d3f4ceadc97744
Alisha Kiran <alishasatheesan1992@gmail.com>
2024-10-23 10:32:31 +0400

    responsive design related changes

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Success = () => {
   return (
-    <div>
+    <div className="homeBody">
         <h2 className="homeTitle ">Your form is submitted sucessfully!</h2>
         <p className="homeMessage">Our team will connect you shortly through provided email or phone number.</p>
     </div>
